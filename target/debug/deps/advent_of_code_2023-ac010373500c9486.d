80f7e402204e3667b3855bef96aec8343fe52769
Red Lattice <114372638+Red-Lattice@users.noreply.github.com>
2023-12-02 10:49:21 -0500

    Delete target directory
    I forgor gitignore

--- target/debug/deps/advent_of_code_2023-ac010373500c9486.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Ian\projects\advent_of_code_2023\target\debug\deps\advent_of_code_2023-ac010373500c9486.rmeta: src\main.rs src\solution_1\mod.rs src\solution_1\solution_1.rs src\solution_1\problem_1_input.txt
-
-C:\Users\Ian\projects\advent_of_code_2023\target\debug\deps\advent_of_code_2023-ac010373500c9486.d: src\main.rs src\solution_1\mod.rs src\solution_1\solution_1.rs src\solution_1\problem_1_input.txt
-
-src\main.rs:
-src\solution_1\mod.rs:
-src\solution_1\solution_1.rs:
-src\solution_1\problem_1_input.txt:
--- target/debug/deps/advent_of_code_2023-cf318cc5a5521203.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Ian\projects\advent_of_code_2023\target\debug\deps\advent_of_code_2023-cf318cc5a5521203.rmeta: src\main.rs src\solution_1\mod.rs src\solution_1\solution_1.rs src\solution_1\problem_1_input.txt
-
-C:\Users\Ian\projects\advent_of_code_2023\target\debug\deps\advent_of_code_2023-cf318cc5a5521203.d: src\main.rs src\solution_1\mod.rs src\solution_1\solution_1.rs src\solution_1\problem_1_input.txt
-
-src\main.rs:
-src\solution_1\mod.rs:
-src\solution_1\solution_1.rs:
-src\solution_1\problem_1_input.txt:
